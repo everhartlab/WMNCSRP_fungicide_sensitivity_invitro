c24a9efc93418fa646f640c557eceec91273469a
Edgar Nieto <edgar.nieto@huskers.unl.edu>
2018-10-07 23:53:19 -0500

    take out dose and concentration what do not need

--- Script_TM.R
+++ Script_TM.R
@@ -3,13 +3,17 @@ library(tidyverse)
 library(ggplot2)
 library(ezec)
 library(NRES803)
+library(broom)
 #READING AND SUBSETTING DATA
 TMdata <- read.csv("data/TM_serialdi_BL-WMN_survey-WMNCSRP(validationdata).csv")
 # Deleting columns
 TMdata <- subset(TMdata, select = - ( X: X.6 ))
 # Setting 4 repeats for each treatment
-TMdata$repeats <- rep_len(1:4,length.out = 2184)
+TMdata$repeats <- rep_len(1:4,length.out = 3192)
 # Getting growth as an average between ecuatorial and polar
+TMdata <- TMdata %>%  
+  group_by(ID, experimental_replicate, dose, ecuatorial, polar, repeats) %>%
+  filter( !experimental_replicate ==1 | !dose== 1.5 | !repeats== 3 )
 TMdata <-
   TMdata %>% mutate(growth = ((ecuatorial + polar) / 2))
 # Creating a function for using the outputs of boxplot.stats function and notice the outlayers
@@ -22,12 +26,17 @@ get_range <- function(mynumber){
 }
 # Using the function in order to take out the outlayers
 TM_filtered <- TMdata %>%
-  group_by(ID, experimental_replicate, dose) %>%
+  group_by(ID, dose) %>%
   mutate(growth_range = list(get_range(growth))) %>%
   unnest() %>%
   filter(growth <= upper & growth >= lower) %>%
   rename(response = growth) %>% 
   ungroup()
+
+#filtering experiment 2 & 3
+TM_filtered <- TM_filtered %>%
+  group_by (ID, dose, repeats, response) %>%
+  filter(experimental_replicate==2 | experimental_replicate==3)
 #Plotting data
 p <- ggplot(TM_filtered, aes(x = ecuatorial, y = polar, fill= ID))
 p +  geom_boxplot(notch = TRUE, aes(group =dose, experimental_replicate), outlier.colour = "#000000") + facet_wrap(~ ID)  +labs(title="Plot of dose ~ response by experimental_replicate", x ="Dose (ppm)", y = "Response (cm)")
@@ -61,53 +70,59 @@ names(TM_xx)[names(TM_xx) == "sample"] <- "ID"
 final_TM<-left_join (RG, TM_xx) %>% mutate(logEC50=(log(Estimate.50)))
 pdf("TM_assumptions_linearmodel_each_dose.pdf")
 # Linear model of log EC50 and relative growth at 0.75ppm, check normality and homogeneity of variancesfinalRG0.75 <- lm(logEC50~RG0.75,final_TM) 
-summary(finalRG0.75)
-check_assumptions(finalRG0.75)
+# summary(finalRG0.75)
+# check_assumptions(finalRG0.75)
     # Linear Regression graph oflog EC50 and relative growth at 0.75 ppm  
-ggplot(finalRG0.75,aes(x=logEC50, y=RG0.75))+  geom_point() + geom_smooth(method = "lm")
+# ggplot(finalRG0.75,aes(x=logEC50, y=RG0.75))+  geom_point() + geom_smooth(method = "lm")
 # Linear model of log EC50 and relative growth at 1 ppm, check normality and homogeneity of variances
 finalRG1 <- lm(logEC50~RG1,final_TM) 
 summary(finalRG1)
+plot(finalRG1)
 check_assumptions(finalRG1)
     # Linear Regression graph of log EC50 and relative growth at 1 ppm 
-ggplot(finalRG1,aes(x=logEC50, y=RG1))+  geom_point() + geom_smooth(method = "lm")
+ggplot(finalRG1,aes(x= RG1, y= logEC50))+  geom_point() + geom_smooth(method = "lm")
 
 # Linear model of log EC50 and relative growth at 1.5 ppm, check normality and homogeneity of variances
 finalRG1.5 <- lm(logEC50~RG1.5,final_TM) 
 summary(finalRG1.5)
+plot(finalRG1.5)
 check_assumptions(finalRG1.5)
     # Linear Regression graph of log EC50 and relative growth at 1.5 ppm
-ggplot(finalRG1.5,aes(x=logEC50, y=RG1.5))+  geom_point() + geom_smooth(method = "lm")
+ggplot(finalRG1.5,aes(x=RG1.5, y=logEC50))+  geom_point() + geom_smooth(method = "lm")
 
 
 # Linear model of log EC50 and relative growth at 2 ppm, check normality and homogeneity of variances
 finalRG2 <- lm(logEC50~RG2,final_TM) 
 summary(finalRG2)
+plot(finalRG2)
 check_assumptions(finalRG2)
       # Linear Regression graph of log EC50 and relative growth at 2 ppm
-ggplot(finalRG2,aes(x=logEC50, y=RG2))+  geom_point() + geom_smooth(method = "lm")
+ggplot(finalRG2, aes(x = RG2, y = logEC50RG2)) +  geom_point() + geom_smooth(method = "lm")
 
 # Linear model of log EC50 and relative growth at 2.5 ppm, check normality and homogeneity of variances
 
 finalRG2.5<- lm(logEC50~RG2.5,final_TM) 
 summary(finalRG2.5)
+plot(finalRG2.5)
 check_assumptions(finalRG2.5)
       # Linear Regression graph of log EC50 and relative growth at 2.5 ppm
-ggplot(finalRG2.5,aes(x=logEC50, y=RG2.5))+  geom_point() + geom_smooth()
+ggplot(finalRG2.5, aes(x = RG2.5, y = logEC50)) +  geom_point() + geom_smooth()
 
 #Dose chosen as DD
 # Linear model of log EC50 and relative growth at 10 ppm, check normality and homogeneity of variances
 finalRG10<- lm(logEC50~RG10,final_TM) 
 summary(finalRG10)
+plot(finalRG10)
 check_assumptions(finalRG2.5)
     # Linear Regression graph of log EC50 and relative growth at 10 ppm
-ggplot(finalRG10,aes(x=logEC50, y=RG10))+  geom_point() + geom_smooth()
+ggplot(finalRG10, aes(x = RG10, y = logEC50)) +  geom_point() + geom_smooth()
 
 # Getting the EC50DD according to the model
 final_TM_DD <- final_TM %>% mutate(Estimate.50DD = exp(1.19249 - (0.02646*RG10)))
 # Linear model of EC50  and EC50DD, check normality and homogeneity of variances
 final_TM_DD_10 <- lm (Estimate.50DD ~ Estimate.50, final_TM_DD)
 summary(final_TM_DD_10)
+plot(final_TM_DD_10)
 check_assumptions(final_TM_DD_10)
 pdf("Linear regression of thiophanate methyl 10 ppm.pdf")   
 # Linear Regression graph of EC50 and EC50DD
